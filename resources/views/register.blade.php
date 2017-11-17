fd559586d26e9aa200dcda277b56071573b677c0
nabil <nabilirfan43@yahoo.com>
2017-11-18 01:38:14 +0800

    second commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,19 +1,23 @@
+@include('inc.header')
+
 <div class="container">
 <div class="row">
     <div class="col-md-8">
-        <div class="slider"></div>
-        <!--divided column into 6/6-->
+      
         <div class="col-md-6">
-            <img src="img/2.jpg" style="width: 150px;height: 150px;" class="img-thumbnail" />
-            <img src="img/2.jpg" style="width: 150px;height: 150px;" class="img-thumbnail"/>
-            <img src="img/2.jpg" style="width: 150px;height: 150px;" class="img-thumbnail"/>
-        </div>
-        <div class="col-md-6">
+        <h1 class:"text-center">Sign Up</h1>
             <p>some content with image</p>
         </div>
+        <div class="col-md-6">
+        <h1 class:"text-center">Sign Up</h1>
+            <p>some content with image</p>
+        </div>
+        <!-- <div class="col-md-6">
+            <p>some content with image</p>
+        </div> -->
     </div>
-    <div class="col-md-4">
+    <!-- <div class="col-md-4">
         some content plus image
-    </div>
+    </div> -->
 </div>
 </div>
